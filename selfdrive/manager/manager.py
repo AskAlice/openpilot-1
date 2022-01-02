1563ae9f5712c5026c329f8b986fa0dd23e7f7bd
Alice Knag <alice@section.io>
2022-01-02 13:09:42 -0700

    running

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -204,7 +204,7 @@ def manager_thread():
 
     running = ' '.join("%s%s\u001b[0m" % ("\u001b[32m" if p.proc.is_alive() else "\u001b[31m", p.name)
                        for p in managed_processes.values() if p.proc)
-    print(running)
+    # print(running)
     cloudlog.debug(running)
 
     # send managerState
